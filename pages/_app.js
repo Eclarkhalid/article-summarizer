fe96334c9824b0b138fc819dcf7959d1116a0387
Eclarkhalid <eclarkhalid@gmail.com>
2023-12-05 09:55:09 +0300

    fixing error 2

--- pages/_app.js
+++ pages/_app.js
@@ -4,7 +4,6 @@ import { Inter } from 'next/font/google'
 import { Provider } from 'react-redux'
 import { store } from './services/store'
 import { Toaster } from 'react-hot-toast'
-import Footer from './components/Footer'
 import Header from './components/Header'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -13,14 +12,13 @@ export default function App({ Component, pageProps }) {
   return <>
     <Provider store={store}>
       <Header />
-      <div className="max-sm:px-4">
+      <main className={`max-sm:px-4 ${inter.className}`}>
         <Component {...pageProps} />
-      </div>
+      </main>
       <Toaster
         position="top-center"
         reverseOrder={false}
       />
-      {/* <Footer /> */}
     </Provider>
   </>
 }
