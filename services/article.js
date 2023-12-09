6e530a279e26efbd956a171b4c2d4eed31ddb88a
Eclarkhalid <eclarkhalid@gmail.com>
2023-12-09 12:23:47 +0300

    pushing changes

--- services/article.js
+++ services/article.js
@@ -1,13 +1,13 @@
 // Import necessary libraries
+import rapidApiKey from '@/config';
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
-const rapidApiKey = process.env.RAPID_API_KEY;
 
 export const articleApi = createApi({
   reducePath: 'articleApi',
   baseQuery: fetchBaseQuery({
     baseUrl: 'https://article-extractor-and-summarizer.p.rapidapi.com/',
     prepareHeaders: (headers) => {
-      headers.set('X-RapidAPI-Key', '8eb4ee3e26msh118e6042122bc7bp1444dajsn0abfae20b0bc');
+      headers.set('X-RapidAPI-Key', rapidApiKey);
       headers.set('X-RapidAPI-Host', 'article-extractor-and-summarizer.p.rapidapi.com');
       return headers;
     }
